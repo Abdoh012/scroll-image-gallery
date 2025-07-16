e1325e4ae6f2edd0da4a385377e7304ec673fd9a
Abdelrahman Mohammed <147283151+Abdoh012@users.noreply.github.com>
2025-07-16 21:01:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Variables
+const back = document.querySelector(".back"); // Back button
+const foeward = document.querySelector(".foeward"); // Forward button
+const imgsContainer = document.querySelector(".imgs"); // Images container
+const swiper = document.querySelector(".swiper");
+const swiperImgs = document.querySelectorAll(".swiper img"); // Swiper images
+
+let movement = 0;
+// End of variables
+
+function backInImages() {
+  swiper.scrollBy(230, 0);
+}
+
+function forwardInIamges() {
+  swiper.scrollBy(-230, 0);
+}
+
+function colredAndbiggerImgs() {}
+// End of functions
+
+// mouseover event => swiper images
+swiperImgs.forEach((img) => {
+  img.addEventListener("mouseover", () => {
+    img.style.cssText = "transform: scale(1.1); filter: none;";
+  });
+});
+
+// mouseleave event => swiper images
+swiperImgs.forEach((img) => {
+  img.addEventListener("mouseleave", () => {
+    img.style.cssText = "transform: scale(1)";
+  });
+});
